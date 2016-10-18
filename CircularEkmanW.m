13b0f77e0b4d8daf36392eeae2deb5255ec2de56
Jacob Wenegrat <jwenegrat@gmail.com>
2016-10-18 15:08:39 -0700

    Cleaned up Circular Code.

--- CircularEkmanW.m
+++ CircularEkmanW.m
@@ -1,24 +1,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CIRCULAR EKMAN TRANSPORT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generates Figure 3 for NL Ekman Manuscript
+% Following Analytical solution of Section 3
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% PARAMETERS
 epsilon = 0.05;
 tau = .1/1035;
 f=1e-4;
-cr = 75e3;
-out = calcCircW(-epsilon, tau, f,cr);
+cr = 75e3; % Radius of Eddy
+out = calcCircW(-epsilon, tau, f,cr); % Calculate (epsilon < 0 is anticyclone)
 X = out.x; Y = out.y; MUBAR = out.MU; MVBAR = out.MV; W = out.W; Wclassic = out.WClassic; UMAG= out.UMAG;
-%%
-% vlim = [-1.25 -.85];
-% wlim = [-1 1]*51e-5;
-% wlim = [-1 1].*1e-5;
 
-
-%%
+%% PLOT ANTICYCLONE SIDE
 figure
-nc = 30;20;24;16;
-
+nc = 30;20;24;16; % Number of contours in plot
 xl = [-2.5 2.5];
-
 gap = [.03 .01]; margh = .1; margw = .1;
 
 for i=1:4
@@ -40,77 +38,68 @@ for i=1:4
             conts = linspace(-0.25, 0.25, nc);
             clim = conts([1 end]);
             lstr = '$\frac{w_e}{\tau_o/(\rho fR)}$';
-            pos = [11 13]
+            pos = [11 13];
         case 4
             var = (W - Wclassic)./(2*epsilon*tau./(f*cr));
             clim = conts([1 end]);
             conts = [-0.3 linspace(-0.25, 0.25, nc) 0.3];
-            
             lstr = '$\frac{w''}{2\epsilon\tau_o/(\rho fR)}$';
             pos = [15 17];
 %             conts = wcontsclass;
     end
 subtightplot(9,2,pos, gap, margh, margw);
-[c, h] = contourf(X./cr, Y./cr, var, conts); shading interp
-% set(h, 'edgecolor', 'none');
-axis square
-% colorbar;
-set(gca, 'xlim', xl, 'ylim', xl);
-% grid on
-ylabel(lstr, 'Rotation', 0, 'Interpreter', 'Latex');
-set(get(gca, 'ylabel'), 'Position', [-4.5260   -0.5504         0]);
-% hold on; [c, h] = contour(X./cr, Y./cr, UMAG, uconts, '-k'); hold off
-hold on; 
-    plot(cos(0:.1:2*pi), sin(0:.1:2*pi), '--k'); 
-    plot(2.*cos(0:.1:2*pi), 2.*sin(0:.1:2*pi), '--k'); 
-%     plot(0.25.*cos(0:.1:2*pi), 0.25*sin(0:.1:2*pi), 'k'); 
-hold off
-grid on
-set(gca, 'clim', clim);
-set(gca, 'XTick', -2:1:2, 'YTick', -2:1:2);
-set(gca, 'FontSize', 12);
-set(get(gca, 'ylabel'), 'FontSize', 18);
-% if i==1; title('Anticyclone'); end
+    [c, h] = contourf(X./cr, Y./cr, var, conts); shading interp
+    % set(h, 'edgecolor', 'none');
+    axis square
+    % colorbar;
+    set(gca, 'xlim', xl, 'ylim', xl);
+    % grid on
+    ylabel(lstr, 'Rotation', 0, 'Interpreter', 'Latex');
+    set(get(gca, 'ylabel'), 'Position', [-4.5260   -0.5504         0]);
+    % hold on; [c, h] = contour(X./cr, Y./cr, UMAG, uconts, '-k'); hold off
+    hold on; 
+        plot(cos(0:.1:2*pi), sin(0:.1:2*pi), '--k'); 
+        plot(2.*cos(0:.1:2*pi), 2.*sin(0:.1:2*pi), '--k'); 
+    hold off
+    grid on
+    set(gca, 'clim', clim);
+    set(gca, 'XTick', -2:1:2, 'YTick', -2:1:2);
+    set(gca, 'FontSize', 12);
+    set(get(gca, 'ylabel'), 'FontSize', 18);
 end
 set(gcf,'Color', 'w', 'Position', [384   130   613   843])
 
 
-%%
+%% VELOCITY STRUCTURE PLOT
 ind = find(out.y == 0);
 
 uvels = out.UMAG(:, ind);
 uvels(1:ind) = -uvels(1:ind);
 uvels(ind) = 0;
-
 om = out.OMEGA(:,ind);
-zetas = out.ZETA(:,ind) - om;
+zetas = out.ZETA(:,ind) - om; % Shear vorticity
+
 subtightplot(9, 2, 1, gap, margh, margw);
-plot(X./cr, uvels./(epsilon.*f.*cr), 'LineWidth', 2);
-hold on
-plot(X./cr, om./(f*epsilon*2), '--');
-plot(X./cr, zetas./(f*epsilon*2), '--');
-hold off
-% axis equal
-set(gca, 'XTick', -2:1:2, 'XLim', xl);
-% set(gca, 'ylim', [0 .4])
-% axis square
+    plot(X./cr, uvels./(epsilon.*f.*cr), 'LineWidth', 2);
+    hold on
+        plot(X./cr, om./(f*epsilon*2), '--');
+        plot(X./cr, zetas./(f*epsilon*2), '--');
+    hold off
+    % axis equal
+    set(gca, 'XTick', -2:1:2, 'XLim', xl);
+        % set(gca, 'ylim', [0 .4])
+        % axis square
+    grid on
+    cb = colorbar;
+    set(cb, 'visible', 'off');
+    pos = get(gca, 'position');
+    % set(gca, 'Position', [1.18 pos(2) 1.375 pos(4)]);
+    set(gca, 'Position', [.191 pos(2) 0.212 pos(4)]);
+    title('Anticyclone');
+    set(gca, 'FontSize', 12);
 
-grid on
-cb = colorbar;
-set(cb, 'visible', 'off');
-
-% set(gca, 'Units', 'inches');
-pos = get(gca, 'position');
-% set(gca, 'Position', [1.18 pos(2) 1.375 pos(4)]);
-set(gca, 'Position', [.191 pos(2) 0.212 pos(4)]);
-title('Anticyclone');
-set(gca, 'FontSize', 12);
-
-% legend('$\bar{u}/(\epsilon f L)$', '$\Omega/(2\epsilon f)$', '$\zeta_s/(2\epsilon f)$', 'Location', 'EastOutside');
-
-%%
-%Cyclone
-out = calcCircW(epsilon, tau, f, cr);
+%% CYLCONE
+out = calcCircW(epsilon, tau, f, cr);  % Recalculate velocity field
 X = out.x; Y = out.y; MUBAR = out.MU; MVBAR = out.MV; W = out.W; Wclassic = out.WClassic;
 %%
 for i=1:4
@@ -139,90 +128,62 @@ for i=1:4
             var = (W - Wclassic)./(2*epsilon*tau./(f*cr));
             clim = conts([1 end]);
             conts = [-0.3 linspace(-0.25, 0.25, nc) 0.3];
-       
             lstr = '$\frac{w''}{\tau_o/(\rho fR)}$';
             pos = [16 18];
 %             conts = wcontsclass;
     end
 subtightplot(9,2,pos, gap, margh, margw);
-[c, h] = contourf(X./cr, Y./cr, var, conts); shading interp
-% set(h, 'edgecolor', 'none');
-axis square
-cb = colorbar;
-if i==2
-    set(cb, 'Ticks', [clim(1) -1 clim(end)]);
-else
-    set(cb, 'Ticks', [clim(1) 0 clim(end)]);
-end
-set(gca, 'xlim', xl, 'ylim', xl);
-% grid on
-% ylabel(lstr, 'Rotation', 0, 'Interpreter', 'Latex');
-% hold on; [c, h] = contour(X./cr, Y./cr, UMAG, uconts, '-k'); hold off
-hold on; 
-    plot(cos(0:.1:2*pi), sin(0:.1:2*pi), '--k'); 
-    plot(2*cos(0:.1:2*pi), 2.*sin(0:.1:2*pi), '--k'); 
-%     plot(0.25.*cos(0:.1:2*pi), 0.25*sin(0:.1:2*pi), 'k'); 
-hold off
-grid on
-set(gca, 'clim', clim);
-set(gca, 'XTick', -2:1:2, 'YTick', -2:1:2);
-set(gca, 'FontSize', 12);
-if i==1;  end
+    [c, h] = contourf(X./cr, Y./cr, var, conts); shading interp
+    % set(h, 'edgecolor', 'none');
+    axis square
+    cb = colorbar;
+    if i==2
+       set(cb, 'Ticks', [clim(1) -1 clim(end)]);
+    else
+        set(cb, 'Ticks', [clim(1) 0 clim(end)]);
+    end
+    set(gca, 'xlim', xl, 'ylim', xl);
+    % grid on
+    % ylabel(lstr, 'Rotation', 0, 'Interpreter', 'Latex');
+    % hold on; [c, h] = contour(X./cr, Y./cr, UMAG, uconts, '-k'); hold off
+    hold on; 
+        plot(cos(0:.1:2*pi), sin(0:.1:2*pi), '--k'); 
+        plot(2*cos(0:.1:2*pi), 2.*sin(0:.1:2*pi), '--k'); 
+    hold off
+    grid on
+    set(gca, 'clim', clim);
+    set(gca, 'XTick', -2:1:2, 'YTick', -2:1:2);
+    set(gca, 'FontSize', 12);
 end
 colormap(flipud(othercolor('RdBu11')));
 set(gcf,'Color', 'w', 'Position', [384   130   613   843])
 
-%%
+%% CYCLONE VELOCITY PLOT
+
 subtightplot(9,2, 2, gap, margh, margw);
-ind = find(out.y == 0);
+    ind = find(out.y == 0);
+    uvels = out.UMAG(:, ind);
+    uvels(1:ind) = -uvels(1:ind);
+    uvels(ind) = 0;
 
-uvels = out.UMAG(:, ind);
-uvels(1:ind) = -uvels(1:ind);
-uvels(ind) = 0;
-
-om = out.OMEGA(:,ind);
-zetas = out.ZETA(:,ind) - om;
-plot(X./cr, uvels./(epsilon.*f.*cr), 'LineWidth', 2);
-hold on
-plot(X./cr, om./(f*epsilon*2), '--');
-plot(X./cr, zetas./(f*epsilon*2), '--');
-hold off
-% axis equal
-set(gca, 'XTick', -2:1:2, 'XLim', xl);
-% set(gca, 'ylim', [0 .4])
-% axis square
-
-grid on
-cb = colorbar;
-set(cb, 'visible', 'off');
-pos = get(gca, 'position');
-set(gca, 'Position', [.5408 pos(2) 0.2150 pos(4)]);
-title('Cyclone');
-set(gca, 'FontSize', 12);
-legend('$\bar{u}/(\epsilon f R)$', '$\Omega/(2\epsilon f)$', '$\zeta_s/(2\epsilon f)$', 'Location', 'EastOutside');
-%% EXPORTING
+    om = out.OMEGA(:,ind);
+    zetas = out.ZETA(:,ind) - om;
+    plot(X./cr, uvels./(epsilon.*f.*cr), 'LineWidth', 2);
+    hold on
+        plot(X./cr, om./(f*epsilon*2), '--');
+        plot(X./cr, zetas./(f*epsilon*2), '--');
+    hold off
+    % axis equal
+    set(gca, 'XTick', -2:1:2, 'XLim', xl);
+    % set(gca, 'ylim', [0 .4])
+    % axis square
+    grid on
+    cb = colorbar;
+    set(cb, 'visible', 'off');
+    pos = get(gca, 'position');
+    set(gca, 'Position', [.5408 pos(2) 0.2150 pos(4)]);
+    title('Cyclone');
+    set(gca, 'FontSize', 12);
+    legend('$\bar{u}/(\epsilon f R)$', '$\Omega/(2\epsilon f)$', '$\zeta_s/(2\epsilon f)$', 'Location', 'EastOutside');
+%% EXPORTING - TO EXPORT USE THIS COMMAND:
 % export_fig('CircularWNew.eps', '-eps', '-opengl', '-q100', '-p01')
-%%
-% colormap(flipud(othercolor('RdBu11')));
-% clear cmnew
-% cm= colormap;
-% cmnew(1:32,:) = cm(1:32,:);
-% cmnew(34:65,:) = cm(33:end,:);
-% cmnew(33,:) = [1 1 1];
-%%
-% wld = wlim(end)/2;
-% wconts = linspace(-wld, wld, 20);
-% figure
-% contourf(X./cr, Y./cr, W-Wclassic, wconts); shading interp
-% axis square
-% 
-% uconts = -(0:.1:.6);
-% % uconts = [.5 .5];
-% hold on
-% [c, h] = contour(X./cr, Y./cr, UMAG, uconts, 'k');
-% clabel(c,h)
-% hold off
-% set(gca, 'xlim', [-1 1], 'ylim', [-1 1]);
-% set(gca, 'clim', wlim/2);
-% grid on
-% title(['\epsilon_{sh} = ', num2str(epsilon), '   \epsilon_k = ', num2str(maxom./f)]);
--- calcCircW.m
+++ calcCircW.m
@@ -1,26 +1,39 @@
 function out = calcCircW(epsilon, tau, f, cr)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CalcCircW
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function calculates the NL Ekman velocities at a circular eddy
+%
+% Inputs: 
+%       epsilon - ubar/(fL)
+%       tau     - tau_o/rho (uniform tau)
+%       f       - Coriolis
+%       cr      - Radius in meters
+%
+%       out = structure of variables
+%
+%   AUTHOR: Jacob O. Wenegrat (jwenegrat@stanford.edu) 10/18/16
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+thetas = 0:.005:2*pi; % Define angles to evaluate
+r = (0.01:.01:1).*3.5*cr; % Define radial coordinates to evaluate
 
-thetas = 0:.005:2*pi;
-% cr = 60e3;
-r = (0.01:.01:1).*3.5*cr;
+ubarmax = epsilon.*f.*cr; % Infer max velocity from inputs
+jetwidth = cr;  % Assuming that the width of jet is set by eddy radius
 
-ubarmax = epsilon.*f.*cr;
-jetwidth = cr;
-
-% Gaussian SSH
+% Gaussian SSH - Assuming a Gaussian SSH gives this vel profile:
 velstruct = r./(jetwidth^2).*exp( - 1/2*(r/(jetwidth)).^2);
 velstruct = velstruct/max(abs(velstruct));
 velstruct = ubarmax.*velstruct;
-dudr = (1-r.^2./jetwidth.^2).*velstruct./r;
+dudr = (1-r.^2./jetwidth.^2).*velstruct./r; % Radial derivative of vel
 
 disp(['Maximum \zeta_s = ', num2str(max(abs(dudr)))]);
 disp(['Maximum \zeta_c = ', num2str(max(abs(velstruct./r)))]);
-%Gaussian Vel
+% Gaussian Vel - Uncomment to assume a Gaussian vel profile
 % velstruct = ubarmax.*exp(- ((r-cr)/jetwidth).^2);
 % dudr = -(r-cr).*2./(jetwidth).^2.*velstruct;
 
-
+% PreAllocating variables
 x = NaN(length(r), length(thetas));
 y = x;
 Myc = x;
@@ -33,37 +46,43 @@ Myeps = x;
 Mxeps = x;
 Mvclass = x;
 Mvlin = x;
-maxom = 0;
-for i=1:length(r)
-   omega = velstruct(i)./r(i);
-   if (abs(omega)>maxom); maxom = abs(omega); end
-   zeta =  dudr(i) + omega;
-   
-   denom = (f+2*omega).*(f+zeta)-omega.^2;
+% maxom = 0;
 
-   Mx(i,:) = (zeta - 2*omega)./denom.*sin(thetas).*cos(thetas).*tau;
-   My(i,:) = -(f + omega + 2.*omega.*sin(thetas).^2 + zeta.*cos(thetas).^2)./denom.*tau;
+% Iterate through radial position
+for i=1:length(r)
+   omega = velstruct(i)./r(i); % Curvature vorticity
+%    if (abs(omega)>maxom); maxom = abs(omega); end
+   zeta =  dudr(i) + omega; % Total vorticity
+   
+   % Following Equations 12-15 in Wenegrat and Thomas 2016
+   denom = (f+2*omega).*(f+zeta)-omega.^2;
+   
+    % Note that these definitions are equivalent to the version given below. 
+%    Mx(i,:) = (zeta - 2*omega)./denom.*sin(thetas).*cos(thetas).*tau;
+%    My(i,:) = -(f + omega + 2.*omega.*sin(thetas).^2 + zeta.*cos(thetas).^2)./denom.*tau;
    
    Mr(i,:) = - (f + 3*omega)./denom.*sin(thetas).*tau;
    Mth(i,:) = -(f + omega+zeta)./denom.*cos(thetas).*tau;
-
+ 
+   %Transport Equations
    Mx(i,:) = Mr(i,:).*cos(thetas) - Mth(i,:).*sin(thetas);
    My(i,:) = Mr(i,:).*sin(thetas) + Mth(i,:).*cos(thetas);
    
-   Myc(i,:) = -tau./(f+zeta).*ones(size(thetas));%% This is wrong, need to consider angle!
+   Myc(i,:) = -tau./(f+zeta).*ones(size(thetas)); % Classic NL Ekman transport
    
+   % Also doing the order epsilon expansions
    Myeps(i,:) = -(f - (zeta-omega).*sin(thetas).^2 - omega.*cos(thetas).^2).*tau./f.^2;
-   Mxeps(i,:) = (zeta-2*omega).*sin(thetas).*cos(thetas).*tau;
+   Mxeps(i,:) = (zeta-2*omega).*sin(thetas).*cos(thetas).*tau; % Over f?
    
-   Mvclass(i,:) = - tau./(f+zeta);
-   Mvlin(i,:) = -tau./f;
+%    Mvclass(i,:) = - tau./(f+zeta); % Classic NL transport relation
+   Mvlin(i,:) = -tau./f; % Classic Ekman transport
    
-   [x(i,:), y(i,:)] = pol2cart(thetas, r(i));
-   uc(i,:) = velstruct(i).*(-sin(thetas));
+   [x(i,:), y(i,:)] = pol2cart(thetas, r(i)); % Convert to cartesian coordinates
+   uc(i,:) = velstruct(i).*(-sin(thetas)); % Find the balanced flow in cart
    vc(i,:) = velstruct(i).*cos(thetas);
-   umag(i,:) = velstruct(i).*ones(size(thetas));
-   omegaf(i,:) = omega;
-   zetaf(i,:) = zeta;
+   umag(i,:) = velstruct(i).*ones(size(thetas)); %Not quite right, but works fine for these purposes
+   omegaf(i,:) = omega; % Save the Omega field
+   zetaf(i,:) = zeta;   % Save the zeta field
 end
 
 nr = length(r); nth = length(thetas);
@@ -73,20 +92,21 @@ yvec = reshape(y, nr*nth, 1);
 Mxvec = reshape(Mx, nr*nth, 1);
 Myvec = reshape(My, nr*nth, 1);
 
-x = (-1:.02:1).*r(end); y = (-1:.02:1).*r(end);
+x = (-1:.02:1).*r(end); y = (-1:.02:1).*r(end); % New coordinates for regridding
 deltax = x(2)-x(1); deltay=y(2)-y(1);
 [X, Y] = meshgrid(x, y);
 
+% Regrid Ekman Transports
 MUBAR = griddata(xvec, yvec, Mxvec, X, Y);
 MVBAR = griddata(xvec, yvec, Myvec, X, Y);
-[MUx, MUy] = gradient(MUBAR, deltax);
+[MUx, MUy] = gradient(MUBAR, deltax); % Tested this order, correct, 10/18/16
 [MVx, MVy] = gradient(MVBAR, deltay);
-W = MUx+MVy;
+W = MUx+MVy; % Ekman vertical vel
 
-Mycvec = reshape(Myc, nr*nth, 1);
+Mycvec = reshape(Myc, nr*nth, 1); %Classic NL Ek transport
 MVBARC = griddata(xvec, yvec, Mycvec, X, Y);
 [MVx, MVy] = gradient(MVBARC, deltay);
-Wclassic = MVy;
+Wclassic = MVy; % Classic NL Pumping Vel
 
 omegavec = reshape(omegaf, nr*nth,1);
 zetavec = reshape(zetaf, nr*nth,1);
@@ -95,13 +115,12 @@ ZETA = griddata(xvec, yvec, zetavec, X, Y);
 
 umagvec = reshape(umag, nr*nth, 1);
 UMAG = griddata(xvec, yvec, umagvec, X, Y);
-
 myepsvec = reshape(Myeps, nr*nth, 1);
 MVEPS = griddata(xvec, yvec, myepsvec, X, Y);
 mxepsvec = reshape(Mxeps, nr*nth, 1);
 MUEPS = griddata(xvec, yvec, mxepsvec, X, Y);
-mvclassvec = reshape(Mvclass,nr*nth,1);
-MVCLASS = griddata(xvec, yvec, mvclassvec, X, Y);
+% mvclassvec = reshape(Mvclass,nr*nth,1);
+% MVCLASS = griddata(xvec, yvec, mvclassvec, X, Y);
 mvlinvec = reshape(Mvlin, nr*nth,1);
 MVLIN = griddata(xvec, yvec, mvlinvec, X, Y);
 
@@ -115,6 +134,7 @@ ZETACART = VX-UY;
 MVCLASS = -tau./(f+ZETACART);
 % Wclassic = UX+VY;
 
+% Put output variables in struct
 out.x = x; out.y = y;
 out.UMAG = UMAG;
 out.MU = MUBAR;
@@ -125,6 +145,6 @@ out.OMEGA = OMEGA;
 out.ZETA = ZETA;
 out.MVEPS = MVEPS;
 out.MUEPS = MUEPS;
-out.MVCLASS= MVCLASS;
+% out.MVCLASS= MVCLASS; %Not sure about this calculation, but not using it.
 out.MVLIN = MVLIN;
 end
